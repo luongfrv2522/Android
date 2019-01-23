d775ecf7c7d6d55d11a96d72b59a0981edca2d7c
Lương <luongfrv2522@gmail.com>
2019-01-23 09:02:16 +0700

    sync

--- app/src/main/java/com/example/luong/location/common/Libs.java
+++ app/src/main/java/com/example/luong/location/common/Libs.java
@@ -4,31 +4,24 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.provider.Settings;
-<<<<<<< HEAD
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Libs {
     @SuppressLint("HardwareIds")
     public static String getAndroidId(Context context){
         return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
     }
-    public static String[] getAndroidHardWare(){
+    public static String[] getAndroidHardWare() {
         String androidSDK = String.valueOf(android.os.Build.VERSION.SDK_INT);
         String androidVersion = android.os.Build.VERSION.RELEASE;
         String androidBrand = android.os.Build.BRAND;
         String androidManufacturer = android.os.Build.MANUFACTURER;
         String androidModel = android.os.Build.MODEL;
         return new String[]{androidSDK, androidVersion, androidBrand, androidManufacturer, androidModel};
-=======
-import android.telephony.TelephonyManager;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import static android.content.Context.TELEPHONY_SERVICE;
-import static android.support.v4.content.ContextCompat.getSystemService;
-
-public class Libs {
+    }
     public static boolean isJSONValid(String test) {
         try {
             new JSONObject(test);
@@ -42,6 +35,5 @@ public class Libs {
             }
         }
         return true;
->>>>>>> master
     }
 }
